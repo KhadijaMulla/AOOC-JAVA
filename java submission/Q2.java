aeedb4d34e81ff1dff09aaf64d4cc73f19f6e843
KhadijaMulla <khadijamulla93493@gmail.com>
2026-02-06 00:12:05 +0530

    Add files via upload

---
+++ java submission/Q2.java
@@ -0,0 +1,22 @@
+public class Q2 {
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        boolean isPrime = true;
+
+        if (n <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= n / 2; i++) {
+                if (n % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime)
+            System.out.println(n + " is a Prime number");
+        else
+            System.out.println(n + " is NOT a Prime number");
+    }
+}
